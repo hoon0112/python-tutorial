d9dbdb0f74a5a569f8276a81078122b32ad6682f
chihoon <specialpw@naver.com>
2021-04-24 19:12:17 +0900

    :tada: 첫 파이썬 코드

---
+++ hello.py
@@ -0,0 +1,24 @@
+print("Hello World!")
+# 나눗셈
+print(5 / 2)
+# 몫/나머지
+print(5 // 2, 5 % 2)
+print(2 ** 3)
+
+print("bitwise 연산")
+print(2 & 1, 1 & 0, 0 & 1, 0 & 0) # AND
+# 2 = 10
+# 1 = 01
+# &-----
+# 0 = 00
+print(1 | 1, 1 | 0, 0 | 1, 0 | 0) # OR
+print(2 ^ 1, 1 ^ 0, 0 ^ 1, 0 ^ 0) # XOR
+# 2 = 10
+# 1 = 01
+# ^-----
+# 3 = 11
+
+print("논리 연산")
+print(2 and 1, 1 and 0, 0 and 1, 0 and 0) # AND
+print(1 or 1, 1 or 0, 0 or 1, 0 or 0) # OR
+print(1 != 1, 1 != 0, 0 != 1, 0 != 0) # XOR
