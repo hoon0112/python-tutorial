d63a44d086fa656f685c6345b2805db0a7245f9b
chihoon <specialpw@naver.com>
2021-04-25 15:30:17 +0900

    :sparkles: Second day of Python

---
+++ variable-io.py
@@ -0,0 +1,13 @@
+# # 변수 대입
+# greeting = "Hello World!"
+# print(greeting)
+
+# # 입력 받기
+# a = int(input("수를 입력하세요: "))
+# b = int(input("또 입력하세요: "))
+# print("합은", a + b, sep=": ")
+
+# # Escape character \
+# print("\n\t\r\"\'")
+
+print("\\n \'")
