a0a15b196d5d0dcd0dffe566fe91a398c68e4949
chihoon <specialpw@naver.com>
2021-05-02 20:18:20 +0900

    if statement

--- baekjoon/1076.py
+++ baekjoon/1076.py
@@ -1,14 +1,33 @@
-def prodval(x): 
+def str_to_num(s):
+    if s == "black":
+        return 0
+    if s == "brown":
+        return 1
+    if s == "red":
+        return 2
+    if s == "orange":
+        return 3
+    if s == "yellow":
+        return 4
+    if s == "green":
+        return 5
+    if s == "blue":
+        return 6
+    if s == "violet":
+        return 7
+    if s == "grey":
+        return 8
+    if s == "white":
+        return 9
+    return -1
+
+ 
+def prodval(x):
     return 10 ** x
 
-a = input()
-b = input()
-c = input()
-black = 0
-brown = 1
-red = 2
-orange = 3
-yellow = 4
-green = 5
 
-print(prodval(green))
+a = str_to_num(input())
+b = str_to_num(input())
+c = str_to_num(input())
+
+print((a * 10 + b) * prodval(c))
