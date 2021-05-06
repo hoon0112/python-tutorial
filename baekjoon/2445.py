18c78c3ad885589089144098c51817e481ffc7f9
chihoon <specialpw@naver.com>
2021-05-06 23:58:03 +0900

    for

---
+++ baekjoon/2445.py
@@ -0,0 +1,6 @@
+n = int(input())
+
+for i in range(n - 1, 0, -1):
+    print("*" * (n - i) + " " * (i * 2) + "*" * (n - i))
+for i in range(n):
+    print("*" * (n - i) + " " * (i * 2) + "*" * (n - i))
